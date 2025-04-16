e12ecc1ea093b04842f907d03c35a239fa270443
Mumin-73 <wjdgyals0514@gmail.com>
2025-04-17 05:00:29 +0900

    Update image2excel.py

--- image2excel.py
+++ image2excel.py
@@ -43,7 +43,7 @@ def imread_unicode(path):
     return cv2.imdecode(stream, cv2.IMREAD_COLOR)
 
 def standardize_dataframe(df):
-    standard_index = ["8:30", "9:00", "9:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
+    standard_index = ["08:30", "09:00", "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30",
                       "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", "16:00", "16:30", "17:00", "17:30"]
     standard_columns = ['월', '화', '수', '목', '금']
     df.index.name = None
